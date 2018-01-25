98a2af463b58a9f4a43a9cb5587978a3b8918f5e
Tyler Welsh <UCDTtbot@users.noreply.github.com>
2018-01-25 17:39:28 +0900

    Droid Cafe Finished

---
+++ TabExperiment/app/src/main/java/shibedays/com/tabexperiment/MainActivity.java
@@ -0,0 +1,56 @@
+package shibedays.com.tabexperiment;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //create instance of the tablayout
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        //Set the text for each tab by adding it
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+        //set the tabs to fill the entire layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Using PagerAdapter to manage page views in fragments.
+        //Each page is represented by its own fragment
+        //This is another example of the adapter pattern
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        //Setting the listener for clicks
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+}
